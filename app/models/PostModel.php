e46909b7dee1f3fd0cb78d0e5b1bb893f6ef8d9a
Shino303 <locshino123@gmail.com>
2024-11-19 08:19:10 +0700

    Update PostModel.php

--- app/models/PostModel.php
+++ app/models/PostModel.php
@@ -4,13 +4,29 @@ class PostModel extends BaseModel
   protected $table = 'posts_table'; // Đặt tên bảng
   protected $column = [
     'id' => 'id',
-    'user_Id' => 'userId',
+    'user_id' => 'userId',
     'title' => 'title',
     'content' => 'content',
-    'media_Type' => 'mediaType',
-    'media_Url' => 'mediaUrl',
+    'media_type' => 'mediaType',
+    'media_url' => 'mediaUrl',
     'status' => 'status',
-    'created_At' => 'createdAt',
-    'updated_At' => 'updatedAt'
+    'created_at' => 'createdAt',
+    'updated_at' => 'updatedAt'
   ];
+
+  public function addPost($data) {
+    $postData = [
+      'userId' => $data['user_id'],
+      'title' => $data['title'],
+      'content' => $data['content'],
+      'mediaType' => $data['media_type'],
+      'mediaUrl' => $data['media_url'],
+    ];
+    return $this->create($postData);
+  }
+
+  public function getNewestPost() {
+    $sql = "SELECT * FROM {$this->table} ORDER BY {$this->column['created_at']} DESC LIMIT 1";
+    return $this->query($sql);
+  }
 }
