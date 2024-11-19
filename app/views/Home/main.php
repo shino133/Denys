32726ca16b2dc396dbaa006e84f359e47b8786f3
Shino303 <locshino123@gmail.com>
2024-11-19 08:13:38 +0700

    Add Admin View

--- app/views/Home/main.php
+++ app/views/Home/main.php
@@ -1,1 +1,10 @@
-<p>Ok</p>
+<div class="newsfeed">
+  <div class="container-fluid" id="wrapper">
+    <div class="row newsfeed-size">
+      <div class="col-md-12 newsfeed-right-side">
+        <?php
+        AppLoader::component("Header/main"); ?>
+      </div>
+    </div>
+  </div> 
+</div>
