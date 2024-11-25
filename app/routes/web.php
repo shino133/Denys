3ae8555e4884490f53f6f5d691d56755659aefed
Shino303 <locshino123@gmail.com>
2024-11-25 08:17:32 +0700

    Update web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -7,9 +7,20 @@ function routeForAdmin()
 
 function routeForUser()
 {
-
   Route::get('/about', 'PageController@about');
   Route::post('/contact', 'ContactController@submit');
+
+  // Post CRUD
+  Route::post('/post/request/add', 'PostController@addPost');
+  Route::post('/post/request/get/{id}', 'PostController@getPost');
+  Route::post('/post/request/edit/{id}', 'PostController@editPost');
+  Route::post('/post/request/destroy/{id}', 'PostController@destroyPost');
+
+  // Asset
+  Route::get('/assets/img/{fileName}', 'AssetController@getImage');
+  Route::get('/assets/js/{fileName}', 'AssetController@getJs');
+  Route::get('/assets/css/{fileName}', 'AssetController@getCss');
+  Route::get('/assets/uploads/{fileName}', 'AssetController@getUpload');
 }
 
 function routeForGuest()
@@ -17,9 +28,11 @@ function routeForGuest()
   Route::get('/', 'HomeController@index');
   Route::get('/logout', 'AuthController@logout');
   Route::get('/404', 'ErrorController@notFoundPage');
+  
   Route::get('/user/login', 'AuthController@login');
   Route::get('/user/register', 'AuthController@register');
   Route::get('/user/logout', 'AuthController@logout');
+
   Route::post('/user/request/login', 'AuthController@loginRequest');
   Route::post('/user/request/register', 'AuthController@registerRequest');
 }
