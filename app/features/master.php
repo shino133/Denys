7974944777ac5f99e0c10d4afbc1a656bbe58f40
Shino303 <locshino123@gmail.com>
2024-11-25 08:18:40 +0700

    Update Features

--- app/features/master.php
+++ app/features/master.php
@@ -1,13 +1,31 @@
 <?php
-include 'app/features/AppLoader.php';
+include 'app/utils/helpers/Loader.php';
+
+// App Loader
+Loader::include('features/AppLoader');
 
 // Supporter
-// AppLoader::helper('core/master');
-AppLoader::helper('master');
+AppLoader::helper('Authentication');
+AppLoader::helper('Action');
+AppLoader::helper('Link');
+AppLoader::helper('Meta');
+AppLoader::helper('Route');
+AppLoader::helper('Store');
+AppLoader::helper('Script');
+AppLoader::helper('Cache');
+AppLoader::helper('Title');
+AppLoader::helper('Url');
+
+AppLoader::feature('Auth');
+
+// Constants
 AppLoader::constant('master');
 
+// Library
+AppLoader::lib('dumpVar');
+
 // Cache setup
-// Cache::configure(__DIR__. '../cache', 3600);
+Cache::configure(__DIR__. '../cache', 3600);
 
 // Library
 // AppLoader::lib('encryptData');
