396883971222aa0b504b62c5495346e481c24085
duyandie <167690825+duyandie@users.noreply.github.com>
2024-11-29 09:05:11 +0700

    Update main.php

--- app/components/Layout/Sidebar/main.php
+++ app/components/Layout/Sidebar/main.php
@@ -1,38 +1,38 @@
 <div class="card newsfeed-user-card h-100">
   <ul class="list-group list-group-flush newsfeed-left-sidebar">
     <li class="list-group-item">
-      <h6>Home</h6>
+      <h6>Trang chủ</h6>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center sd-active">
       <a href="index.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/newsfeed.png" alt="newsfeed" />
-        News Feed</a>
+        Tin Tức Mới</a>
       <a href="#" class="newsfeedListicon"><i class="bx bx-dots-horizontal-rounded"></i></a>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="messages.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/message.png"
           alt="message" />
-        Messages</a>
+        tin nhắn</a>
       <span class="badge span-badge badge-pill">2</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="groups.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/group.png" alt="group" />
-        Groups</a>
+        Nhóm</a>
       <span class="badge span-badge badge-pill">17</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="events.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/event.png" alt="event" />
-        Events</a>
+        Sự kiện</a>
       <span class="badge span-badge badge-pill">3</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="saved.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/saved.png" alt="saved" />
-        Saved</a>
+        Đã lưu</a>
       <span class="badge span-badge badge-pill">8</span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="find-friends.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/find-friends.png"
           alt="find-friends" />
-        Find Friends</a>
+        Tìm bạn bè</a>
       <span class="badge span-badge badge-pill"><i class="bx bx-chevron-right"></i></span>
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
@@ -42,7 +42,7 @@
     </li>
     <li class="list-group-item d-flex justify-content-between align-items-center">
       <a href="teams.html" class="sidebar-item"><img src="/public/img/icons/left-sidebar/team.png" alt="find-friends" />
-        Argon For Teams</a>
+        Argon cho các đội</a>
       <span class="badge span-badge badge-pill"><i class="bx bx-chevron-right"></i></span>
     </li>
   </ul>
