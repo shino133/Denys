9c933308d431b5b828c4a7bc1a3307e4cddec5fc
Shino303 <locshino123@gmail.com>
2024-11-29 21:49:59 +0700

    Update GroupModel.php

--- app/models/GroupModel.php
+++ app/models/GroupModel.php
@@ -12,4 +12,33 @@ class GroupModel extends BaseModel
     'created_at' => 'createdAt',
     'updated_at' => 'updatedAt'
   ];
+
+  public static function getGroups($orderBy = 'created_at', $conditions = ['status' => "active"] ,$limit = 10)
+  {
+    $userTable = "users_table";
+
+    $joins = [
+      [
+        'type' => 'INNER',
+        'table' => $userTable,
+        'on' => "$userTable.id = " . self::$table . ".userId"
+      ],
+    ];
+
+    $columns = [
+      self::$table . ".id as group_id",
+      self::$table . ".name as group_name",
+      self::$table . ".bannerUrl as group_bannerUrl",
+      self::$table . ".status as group_status",
+      self::$table . ".createdAt as group_createdAt",
+      $userTable . ".fullName as user_fullName",
+      $userTable . ".userName as user_userName",
+    ];
+
+    if ($orderBy) {
+      $orderBy = "" . self::$table . "." . self::$columns[$orderBy] . " DESC";
+    }
+
+    return self::join($joins, $columns, $conditions, $orderBy, $limit);
+  }
 }
