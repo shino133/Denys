9555548883a6f9f460cdcbb1d28707440115d350
Shino303 <locshino123@gmail.com>
2024-11-29 22:21:12 +0700

    Update EventModel.php

--- app/models/EventModel.php
+++ app/models/EventModel.php
@@ -12,4 +12,36 @@ class EventModel extends BaseModel
     'created_at' => 'createdAt',
     'updated_at' => 'updatedAt'
   ];
+
+  public static function getEvents($orderBy = 'created_at', $conditions = ['status' => "active"], $limit = 10)
+  {
+    $userTable = "users_table";
+    
+    $joins = [
+      [
+        'type' => 'INNER',
+        'table' => $userTable,
+        'on' => "$userTable.id = " . self::$table . ".userId"
+      ],
+    ];
+    
+    $columns = [
+      self::$table . ".id as event_id",
+      self::$table . ".title as event_title",
+      self::$table . ".bannerUrl as event_bannerUrl",
+      self::$table . ".content as event_content",
+      self::$table . ".status as event_status",
+      self::$table . ".createdAt as event_createdAt",
+      self::$table . ".updatedAt as event_updatedAt",
+      $userTable . ".fullName as user_fullName",
+      $userTable . ".userName as user_userName",
+    ];
+    
+    if ($orderBy) {
+      $orderBy = "" . self::$table . "." . self::$columns[$orderBy] . " DESC";
+    }
+    
+    return self::join($joins, $columns, $conditions, $orderBy, $limit);
+  }
+
 }
