57d9814f0ce5cced0443894b15a067b480fb32c3
Shino303 <locshino123@gmail.com>
2025-01-24 16:43:44 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 session_start();
-require_once __DIR__ .'/../app/autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../app/boostapp/app.php';
+require_once __DIR__.'/../app/routes/master.php';
